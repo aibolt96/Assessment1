7db79ab1966c9cf01d125ecc74b951471128757d
Austin Bolton <aibolt3@gmail.com>
2023-06-24 10:43:11 -0500

    finished sdlc

--- sdlc.js
+++ sdlc.js
@@ -18,4 +18,9 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+const analysisOfRequirements = 'gather information from the client for the requirements, as well as any details and specifications for the product. Developers will analyze the requirements and determine how to incorporate those requirements into the software.'
+const design = 'Developers identifies any key components or defining structures for the product and make a detailed plan for developement.'
+const implementation = 'Developers begin working on creating the product, attempting to incorporate all required details and specifications as efficently as possible.'
+const testingAndIntegration = 'Development team will test the software to attempt to find any kind of error as well as ensure that the developed systems meet all the techincal requirements with all components integrated.'
+const maintain = 'After the software is fully operational, the software is then monitored to make sure it runs as intended, but also upgrade performance, fix bugs among many other things.' 
